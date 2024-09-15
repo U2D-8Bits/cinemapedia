c5059c93f81f3b6c430339681c78e18bd5b5cc34
U2D-8Bits <u2d.8bits@gmail.com>
2024-09-15 15:19:27 -0500

    Creación de Vista MovieScreen y Ruta

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -10,6 +10,14 @@ final appRouter = GoRouter(
       path: '/', 
       name: HomeScreen.routeName,
       builder: (context, state) => const HomeScreen(),
+    ),
+    GoRoute(
+      path: '/movie/:id', 
+      name: MovieScreen.routeName,
+      builder: (context, state) {
+        final movieId = state.pathParameters['id'];
+        return MovieScreen(movieId: movieId!);
+      },
     )
 
   ]
