0ed01ded8739b2657dd1a9aa7b3d4e0dac64586b
U2D-8Bits <u2d.8bits@gmail.com>
2024-09-17 21:05:55 -0500

    BackUp

--- lib/presentation/widgets/shared/custom_bottom_navigationbar.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigationbar.dart
@@ -10,7 +10,7 @@ class BottomNavigationbar extends StatelessWidget {
       elevation: 0,
       items: const [
       BottomNavigationBarItem(icon: Icon(Icons.home_max_outlined), label: 'Inicio'),
-      BottomNavigationBarItem(icon: Icon(Icons.category_outlined), label: 'Categorías'),
+      BottomNavigationBarItem(icon: Icon(Icons.workspace_premium_rounded), label: 'Populares'),
       BottomNavigationBarItem(icon: Icon(Icons.favorite_outline_outlined), label: 'Favoritos'),
     ]);
   }
