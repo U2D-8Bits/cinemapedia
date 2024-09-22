cb8b263f4e303b063a6af29c4d38c1c5ce9ceb70
U2D-8Bits <u2d.8bits@gmail.com>
2024-09-21 23:29:42 -0500

    Diseño de vista para favoriteview cuando no tiene favoritos registrados

--- lib/presentation/screens/views/movies/favorite_view.dart
+++ lib/presentation/screens/views/movies/favorite_view.dart
@@ -2,6 +2,7 @@ import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class FavoriteView extends ConsumerStatefulWidget {
   const FavoriteView({super.key});
@@ -45,6 +46,27 @@ class FavoriteViewState extends ConsumerState<FavoriteView> {
     // Obtenemos las películas favoritas en forma de lista
     final favoriteMoviesList = ref.watch(favoriteMoviesProvider).values.toList();
 
+    if (favoriteMoviesList.isEmpty){
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Icon(Icons.favorite, size: 100, color: Colors.grey),
+            const Text('No tienes películas favoritas', style: TextStyle(color: Colors.grey, fontSize: 20)),
+            const SizedBox(height: 20),
+            FilledButton.tonal(
+              style: const ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(Colors.white60),
+              ) ,
+              onPressed: () => context.push('/home/0'), 
+              child: const Text('Explorar películas', style: TextStyle(color: Colors.black, fontSize: 16)),
+            )
+          ],
+        ),
+      );
+    }
+
 
     return Scaffold(
       body: MoviesMasonry(
@@ -56,3 +78,4 @@ class FavoriteViewState extends ConsumerState<FavoriteView> {
 
   
 }
+
